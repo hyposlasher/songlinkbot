d4ba326228f9463423a40aad8932a54d3c8f8945
Dinislam Maushov <dmaushov@gmail.com>
2021-03-31 22:58:17 +0300

    change

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const TOKEN = '1723140864:AAGZMlFiJTSZqEc1wIH6LIt8ZUP2lKSmHsU';
 
 const options = {
   webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
     port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
 
@@ -86,29 +80,14 @@ bot.on('message', (msg) => {
           }
         })
 
-        // const urls = links.reduce((acc, link, i) => {
-        //   if (i !== 0) {
-        //     return acc + ` | <a href='${link.url}'>${link.name}</a>`
-        //   }
-
-        //   return acc + `<a href='${link.url}'>${link.name}</a>`
-          
-        // }, '');
-
         sendSong(chatId, img, artist, song, links);
       })
       .catch(function (error) {
         if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
           console.log('error.response.data:', error.response.data);
         } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
           console.log('error.request: ', error.request);
         } else {
-          // Something happened in setting up the request that triggered an Error
           console.log('Error', error.message);
         }
       });
