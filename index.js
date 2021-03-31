a07090510159b42cd4249968f8ca1d3fb2dec402
Dinislam Maushov <dmaushov@gmail.com>
2021-03-31 22:39:06 +0300

    change

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ bot.on('message', (msg) => {
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          console.log('error.response.data:', error.response.data);
+          console.log('error.response.data: test', error.response.data);
         } else if (error.request) {
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
